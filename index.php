fd2f93478f0919e2c0a785b1a38d018fded92310
BrandonBaLu <1719110177@utectulancingo.edu.mx>
2020-11-20 15:31:00 +0000

    funcionando

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-    echo "Hola desde PHP";
-?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Agenda</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <h1>Index</h1>
+        <a href="tienda/index.php">Tienda</a>
+    </div>
+</body>
+</html>
